35933e7ca7c55f33f6b58841030b1dec993fb3e1
Shiho-Tajima <konichigo2000@yahoo.co.jp>
2025-09-13 16:43:24 +0900

    デモ用

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ BASE_DIR = os.path.dirname(__file__)
 logo_img_path = os.path.join(BASE_DIR, "data", "images", "logo.png")
 
 # カラム比率1:2:1で作成
-col1, col2 = st.columns([3,1])
+col1, col2 = st.columns([5,1])
 
 with col2:
     st.image(logo_img_path, use_container_width=True)  # 新しい推奨パラメータ
@@ -61,7 +61,7 @@ if not st.session_state["greeted"]:
     ai_img_path = os.path.join(BASE_DIR, "data", "images", "ai.png")
 
     # カラム比率1:2:1で作成
-    col1, col2, col3 = st.columns([2,1,2])
+    col1, col2, col3 = st.columns([4,1,4])
 
     with col2:
         st.image(ai_img_path, use_container_width=True)
